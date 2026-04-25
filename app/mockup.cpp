a17ac5a8587185348d0248fb466a716ab2f19dd4
1mache <miktor4925@gmail.com>
2026-04-25 22:22:53 +0300

    SDL basic setup created

--- app/mockup.cpp
+++ app/mockup.cpp
@@ -1,44 +1,101 @@
 #include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+
 #include <iostream>
 
-int main()
+constexpr int             WIN_WIDTH = 720;
+constexpr int             WIN_HEIGHT = 405;
+constexpr SDL_WindowFlags WIN_FLAGS = 0;
+
+bool createWindowAndRenderer(const char*    title,
+                             SDL_Window*&   window,
+                             SDL_Renderer*& renderer)
 {
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
         std::cerr << "Init error : " << SDL_GetError() << std::endl;
-        return EXIT_FAILURE;
+        return false;
     }
 
-    auto window = SDL_CreateWindow("MCE", 720, 405, SDL_WINDOW_OPENGL);
+    window = SDL_CreateWindow(title, WIN_WIDTH, WIN_HEIGHT, WIN_FLAGS);
 
     if (!window)
     {
         std::cerr << "Window creation error : " << SDL_GetError() << std::endl;
         SDL_Quit();
-        return EXIT_FAILURE;
+        return false;
     }
 
     SDL_SetWindowPosition(window,
                           SDL_WINDOWPOS_CENTERED,
                           SDL_WINDOWPOS_CENTERED);
 
+    renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer)
+    {
+        std::cerr << "Renderer creation error : " << SDL_GetError()
+                  << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return false;
+    }
+
+    return true;
+}
+
+void destroyResourcesAndQuit(SDL_Window* window, SDL_Renderer* renderer)
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main()
+{
+    SDL_Window*   window{};
+    SDL_Renderer* renderer{};
+
+    if (!createWindowAndRenderer("Megaman Mockup", window, renderer))
+        return EXIT_FAILURE;
+
     bool isRunning = true;
+
+
+    SDL_Surface* surface = IMG_Load("res/area.png");
+    if (!surface)
+    {
+        std::cerr << "Image loading error : " << SDL_GetError() << std::endl;
+        destroyResourcesAndQuit(window, renderer);
+        return EXIT_FAILURE;
+    }
+
+    // 1.load scene picture
+    // 2.init player
+    // 3.init enemy
+    // 4.init health indication
+    // 5.init ground collider
+
+
+    SDL_Event* event{};
 
     while (isRunning)
     {
-        // render here
+        // 1.  move player (+ animate)
+        // check player ground position
+        // 2.  move enemy (hover)
+        // 3.  if player at (x1,y1) position => jumps + switch anim.
+        // 4.  if player at (x2,y2) position => shoots + switch anim.
+        // 5.  spawn projectile + move it
+        // 6.  if projectile hit enemy enemy blinks and health -1
+        // 7.  player jumps and shoots again
+        // 8.  repeat 5-6
+        // 9.  enemy dies
+        // 10. spawn + animate explosion
 
-        // input
-        SDL_Event event;
-
-        while (SDL_PollEvent(&event))
+        if (SDL_PollEvent(event))
         {
-            switch (event.type)
-            {
-            case SDL_EVENT_QUIT:
+            if (event->type == SDL_EVENT_QUIT)
                 isRunning = false;
-                break;
-            }
         }
     }
 
