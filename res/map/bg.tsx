e33a5faa5f828e499c51796547c13362f06d55f5
1mache <miktor4925@gmail.com>
2026-05-05 15:47:08 +0300

    Updated sprites

---
+++ res/map/bg.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="bg" tilewidth="64" tileheight="112" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="bg">
+  <image source="bg.png" width="64" height="112"/>
+ </tile>
+</tileset>
